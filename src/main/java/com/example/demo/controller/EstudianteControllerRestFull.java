594763d26e303dbecac570952ec93255a814ed57
Mona <cmoyacharro@gamil.com>
2024-01-24 20:54:08 -0500

    taller26

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -1,5 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+
+//SERVICIO -> Controller: Clase controller
+@RestController
+
 public class EstudianteControllerRestFull {
+	
+	//capaciadades
+	public void guardar(Estudiante estudiante) {
+		
+		
+	}
 
 }
--- src/main/java/com/example/demo/service/IEstudianteService.java
+++ src/main/java/com/example/demo/service/IEstudianteService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
-public class IEstudianteService {
+import com.example.demo.repository.modelo.Estudiante;
 
+public interface IEstudianteService {
+	public void guardar(Estudiante estudiante);
+	public void actualizar(Estudiante estudiante);
+	public void actualizarParcial(String apellido, String nombre, Integer id);
+	public Estudiante buscar(Integer id);
+	public void borrarr(Integer id);
 }
