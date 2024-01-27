9c896a1884d69c7b1d9c678e3ed53b8a4deb692b
Christian Moya <cmoyacharro@gmail.com>
2024-01-27 10:48:08 -0500

    taller 27

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -3,17 +3,28 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
 
 //SERVICIO -> Controller: Clase controller
 @RestController
-
+@RequestMapping(path="/estudiantes")
 public class EstudianteControllerRestFull {
+
+    private IEstudianteService estudianteService;
 	
 	//capaciadades
-	public void guardar(Estudiante estudiante) {
-		
-		
+    //Get
+    @GetMapping(path ="/buscar")
+	public Estudiante buscar() {
+        return this.estudianteService.buscar(1);		
 	}
 
+    //http://localhost:8080/API/v1.0/Matricula/estudiante/buscar
+
+    @PostMapping(path ="/guardar")
+    public void guardar(@RequestBody Estudiante estudiante){
+        this.estudianteService.guardar(estudiante);
+    }
+    //http://localhost:8080/API/v1.0/Matricula/estudiante/guardar
 }
