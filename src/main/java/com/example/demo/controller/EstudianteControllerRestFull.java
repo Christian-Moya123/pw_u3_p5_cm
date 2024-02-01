4eb0bba39d6c49406be4fe4a40df1d908c2b6457
Mona <cmoyacharro@gamil.com>
2024-01-31 19:36:57 -0500

    taller 28 verbos

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -1,19 +1,55 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
+
 
 
 //SERVICIO -> Controller: Clase controller
 @RestController
-
+@RequestMapping(path="/estudiantes")
 public class EstudianteControllerRestFull {
-	
+
+	@Autowired
+  private IEstudianteService estudianteService;
+
 	//capaciadades
-	public void guardar(Estudiante estudiante) {
-		
-		
+  //Get
+  @GetMapping(path ="/buscar")
+	public Estudiante buscar() {
+      return this.estudianteService.buscar(3);		
 	}
 
+  //http://localhost:8080/API/v1.0/Matricula/estudiante/buscar
+
+  @PostMapping(path ="/guardar")
+  public void guardar(@RequestBody Estudiante estudiante){
+      this.estudianteService.guardar(estudiante);
+  }
+  //http://localhost:8080/API/v1.0/Matricula/estudiante/guardar
+  
+  @PutMapping(path = "/actualizar")
+  public void actualizar(@RequestBody Estudiante estudiante) {
+	  this.estudianteService.actualizar(estudiante);
+  }
+  
+  @PatchMapping(path = "/actualizarParcial")
+  public void actualizarParcial(@RequestBody Estudiante estudiante) {
+	  this.estudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), estudiante.getId());
+  }
+  
+  @DeleteMapping(path = "/borrar")
+  public void borrar() {
+	  this.estudianteService.borrarr(22);
+  }
 }
--- src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/EstudianteRepositoryImpl.java
@@ -13,45 +13,35 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class EstudianteRepositoryImpl implements IEstudianteRepository{
 
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	@Override
-	public void insertar(Estudiante estudiante) {
-		// TODO Auto-generated method stub
-		this.entityManager.persist(estudiante);
-	}
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public void actualizar(Estudiante estudiante) {
-		// TODO Auto-generated method stub
-		this.entityManager.merge(estudiante);
-	}
+    @Override
+    public void insertar(Estudiante estudiante) {
+        this.entityManager.persist(estudiante);
+    }
+    @Override
+    public void actualizar(Estudiante estudiante) {
+        this.entityManager.merge(estudiante);
 
-	@Override
-	public void actualizarParcial(String apellido, String nombre, Integer id) {
-		// TODO Auto-generated method stub
-		String sql = "UPDATE Estudiante e SET e.nombre =:valor1, e.apellido =: valor2 WHERE e.id =: valor3";
-		Query query = this.entityManager.createQuery(sql);
-		query.setParameter("valor1", nombre);
-		query.setParameter("valor2", apellido);
-		query.setParameter("valor3", id);
-		query.executeUpdate();
-	}
+    }
+    @Override
+    public void actualizarParcial(String apellido, String nombre, Integer id) {
+        //SQL: UPDATE estudiante  e set e.estu_nombre=:valor, e.estu_apellido=:valor2
+        Query query=this.entityManager
+                .createQuery("UPDATE Estudiante e SET e.nombre = :valor1, e.apellido =:valor2 WHERE e.id =:valor3");
+        query.setParameter("valor1", nombre);
+        query.setParameter("valor2", apellido);
+        query.setParameter("valor3", id);
+        query.executeUpdate();
 
-	@Override
-	public Estudiante seleccionar(Integer id) {
-		// TODO Auto-generated method stub
-		this.entityManager.find(Estudiante.class, id);
-		return null;
-	}
-
-	@Override
-	public void eliminar(Integer id) {
-		// TODO Auto-generated method stub
-		this.entityManager.remove(this.seleccionar(id));
-	}
-	
-	
-
+    }
+    @Override
+    public Estudiante seleccionar(Integer id) {
+        return this.entityManager.find(Estudiante.class, id);
+    }
+    @Override
+    public void eliminar(Integer id) {
+        this.entityManager.remove(this.seleccionar(id));
+    }
 }
