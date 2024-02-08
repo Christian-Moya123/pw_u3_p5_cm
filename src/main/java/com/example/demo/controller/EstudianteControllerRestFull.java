a9c387ae42ebc26949930447dc3e12c8dcfe029a
Mona <cmoyacharro@gamil.com>
2024-02-07 20:36:54 -0500

    taller 31 consume poduces media type

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -3,6 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -29,32 +34,38 @@ public class EstudianteControllerRestFull {
 
 	//capaciadades
   //Get
-	@GetMapping(path = "/{id}")
-	public Estudiante buscar(@PathVariable(name = "id") Integer id) {
-	    return this.estudianteService.buscar(id);
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public ResponseEntity<Estudiante> buscar(@PathVariable(name = "id") Integer id) {
+		
+		//240> grupo satisfactorio
+		//240 rescurso Estudiante encontrado satifactoriamente
+		Estudiante estu = this.estudianteService.buscar(id);
+		//Contrato de la API(`. documentacion, Swagger.io)
+		
+		return ResponseEntity.status(HttpStatus.OK).body(estu);
 	}
 
   //http://localhost:8080/API/v1.0/Matricula/estudiante/buscar
 
-  @PostMapping
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   public void guardar(@RequestBody Estudiante estudiante){
       this.estudianteService.guardar(estudiante);
   }
   //http://localhost:8080/API/v1.0/Matricula/estudiante/guardar
   
-  @PutMapping(path = "/{id}")
+  @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
   public void actualizar(@RequestBody Estudiante estudiante, @PathVariable(name = "id") Integer id) {
 	estudiante.setId(id);
 	  this.estudianteService.actualizar(estudiante);
   }
   
-  @PatchMapping(path = "/{id}")
+  @PatchMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
   public void actualizarParcial(@RequestBody Estudiante estudiante, @PathVariable(name = "id") Integer id) {
 	  this.estudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), estudiante.getId());
   }
   
   //PATHVARIABLE
-  @DeleteMapping(path = "/{id}")
+  @DeleteMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
   public void borrar(@PathVariable(name = "id") Integer id) {
 	  this.estudianteService.borrarr(id);
   }
@@ -63,8 +74,14 @@ public class EstudianteControllerRestFull {
   //REQUESTPARAM
   //consultarTodos?genero=F
   //consultarTodos?genero=F&edad=100
-  @GetMapping
-  public List<Estudiante> consultarTodos(@RequestParam(required = false, defaultValue = "M", name = "genero") String genero) {
-      return this.estudianteService.consultarTodos(genero);
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<Estudiante>>consultarTodos(@RequestParam(required = false, defaultValue = "M", name = "genero") String genero) {
+      List<Estudiante> lista = this.estudianteService.consultarTodos(genero);
+      
+      HttpHeaders cabeceras = new HttpHeaders();
+      cabeceras.add("mensaje_242", "lista consultada de manera satisfactoria");
+      cabeceras.add("mensaje_info", "el sistema va a estar en mantenimiento el fin de semana");
+      
+      return new ResponseEntity<>(lista, cabeceras,242);
   }
 }
